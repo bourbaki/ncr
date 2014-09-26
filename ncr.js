68006c017131987d77ef726afc68ca7f7623d556
Artemka B <bourbaki@me.com>
2014-09-26 14:15:04 +0400

    fixed generalizedLucas, passing current tests

--- ncr.js
+++ ncr.js
@@ -1,7 +1,7 @@
 var modulo  = 142857;
 var factorization = [27, 11, 13, 37];
 
-// FP functions
+// FP functions and extensions
 function zip(xs, ys) {
   var i, length = Math.min(xs.length, ys.length), arr = [];
   for (i = 0; i < length; i += 1) {
@@ -10,13 +10,34 @@ function zip(xs, ys) {
   return arr;
 }
 
+ Array.prototype.product = function () {
+  return this.reduce(function(p, x) {return p*x;}, 1);
+ };
+
+function compose(f, g) {
+  return function () {
+      return f(g.apply(null, arguments));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 function baseRepresentation(n, base, digits) {
 	var rprs = [];
 	while (n > 0){
 		rprs.push(n % base);
 		n = Math.floor(n / base);
 	}
-	console.log(digits); console.log(rprs.length);
 	if (digits !== undefined && digits > rprs.length) {
     rprs = rprs.concat(zeros(digits - rprs.length));
 	}
@@ -24,22 +45,23 @@ function baseRepresentation(n, base, digits) {
 }
 
 function chooseCRT(n, k, fctrz) {
-	return solveCRT(fctrz, fctrz.map(function (e) { return chooseLucas(n, k, e);}));
+  var _flatten = function (x) {return typeof x === 'object' ? Math.pow(x.prime, x.power) : x;}
+	return solveCRT(fctrz.map(_flatten), fctrz.map(function (e) { return chooseLucas(n, k, e);}));
 }
 
 function chooseLucas(n, k, modulo) {
-	console.log("chooseLucas " + n + " " + k + " " + modulo); 
 	if (k > n) return 0;
 	if (k == 0 || k == n) return 1;
 	if (n == 0) return 0;
 	
+	if(typeof modulo === 'object') {
+    return generalizedLucas(n, k, modulo.prime, modulo.power);
+  }
+	
 	var k_m = baseRepresentation(k, modulo),
 		n_m = baseRepresentation(n, modulo);
 	
-	console.log("\t" + n_m);
-	console.log("\t" + k_m);
-		
-	if (k_m.length > n_m.length) return 0;
+  if (k_m.length > n_m.length) return 0;
 	if (k_m.length < n_m.length) n_m = n_m.slice(0, k_m.length);
 	
 	// zip representations
@@ -48,11 +70,11 @@ function chooseLucas(n, k, modulo) {
 	for(i = 0; i < k_m.length; i += 1) {
 		arr.push({n: n_m[i], k: k_m[i]});
 	}
-	console.log(arr);
-	if(!arr.every(function (e) {return e.n >= e.k;})) return 0;
+
+  if(!arr.every(function (e) {return e.n >= e.k;})) return 0;
 	
 	arr = arr.map(function(e) {return choose(e.n, e.k, modulo);});
-	console.log(arr);
+
 	return arr.reduce(function (prev, curr) {return prev * curr;}, 1) % modulo;
 }
 
@@ -133,11 +155,11 @@ function solveCRT(ns, as) {
   return mod(x, N);
 }
 
-function factorialPrime(n, p) {
+function factorialPrime(n, p, modulo) {
   var i;
   var result = 1;
   for (i = 0; i <= n; i += 1) {
-    if(i % p !== 0) result *= i;
+    if(i % p !== 0) result = (result * i) % modulo;
   }
   return result;
 }
@@ -150,31 +172,22 @@ function generalizedLucas(n, k, p, q) {
     .map(function (pj) { return Math.floor( x / pj ) % pq});
   }
   var _fp = function (e) {
-    return factorialPrime(e, p);
+    return factorialPrime(e, p, pq);
   }
   
-  var Ns = _generate(n);
-  var Ks = _generate(k);
-  var Rs = _generate(r);
+  var Ns = _generate(n).map(_fp);
+  var Ks = _generate(k).map(_fp);
+  var Rs = _generate(r).map(_fp);
   
   
+  var numerator = Ns.product() % pq;
+  var denominator = Ks.product() * Rs.product() % pq;
   
-  var i, prs = [];
-  
-  for (i = 0; i < digits; i += 1) {
-    prs.push(Ns[i] / (Ks[i] * Rs[i]));
-  } 
-  
-  var res = prs.reduce(function (prev, curr) {return prev * curr;}, 1);
-  
-  
-  
-  //var arr = [n, k, r].map(_generate);
+  var _tmp = numerator * modularInverse(denominator, pq);
   
   var k_p = baseRepresentation(k, p, digits),
       n_p = baseRepresentation(n, p, digits);
   
-  console.log(n_p); console.log(k_p);
   
   function _computeE(j) {
     return zip(n_p, k_p).slice(j).reduce(function (sum, x) { return sum + (x[0] < x[1]);}, 0);
@@ -184,10 +197,15 @@ function generalizedLucas(n, k, p, q) {
   es[0] = _computeE(0);
   es[q-1] = _computeE(q-1);
   
-  return Math.pow(-1, es[q - 1]) * res * Math.pow(p, es[0]);
-  
-  
+  return Math.pow(-1, es[q - 1]) * _tmp * Math.pow(p, es[0]) % pq;
  }
+ 
+ function modularInverse(a, modulo) {
+  // gcd(a, modulo) should be equal to 1
+  return extendedEuclid(a, modulo).s;
+ }
+ 
+exports.modularInverse = modularInverse;
 exports.baseRepresentation = baseRepresentation;
 exports.chooseLucas = chooseLucas;
 exports.choose = choose;
@@ -196,3 +214,4 @@ exports.factorization = factorization;
 exports.extendedEuclid = extendedEuclid;
 exports.solveCRT = solveCRT;
 exports.generalizedLucas = generalizedLucas;
+exports.compose = compose;
--- test/test.js
+++ test/test.js
@@ -3,6 +3,19 @@ var ncr = require("../ncr.js")
 
 describe('ncr', function() {
 	var answer = 64269;
+	var factorization = [{prime: 3, power: 3}, 11, 13, 37];
+	
+	describe('compose', function () {
+		var _f = function (x) { return x * 2; }
+		var _g = function (a, b) { return a + b; }
+		
+		it('should return correct value', function () {
+				var _w = ncr.compose(_f, _g);
+				var result = _w(1, 1);
+				assert.equal(4, result);
+		})
+	})
+	
 	describe('extendedEuclid', function() {
 		var a = 46, b = 240;
 		var result = ncr.extendedEuclid(a, b);
@@ -13,6 +26,7 @@ describe('ncr', function() {
 			assert.equal(result.gcd, result.s * a + result.t * b);
 		});
 	})
+	
 	describe('solveCRT', function() {
 		it('should exist', function() {
 			assert.equal(true, ncr.hasOwnProperty('solveCRT'));
@@ -25,30 +39,44 @@ describe('ncr', function() {
 			assert.equal(11, result);
 		})
 	});
+	
 	describe('chooseLucas', function() {
-		var factorization = [27, 11, 13, 37];
+		
 		
 		var n = 84, k = 66;
-		var expected = factorization.map(function (x) {return answer % x;})
+		var _flatten = function (x) {return typeof x === 'object' ? Math.pow(x.prime, x.power) : x;}
+		var expected = factorization.map(ncr.compose(function (x) {return answer % x;}, _flatten));
 		it('should return correct remainders', function () {
 			var actual = factorization.map(function (e) {return ncr.chooseLucas(n, k, e);})
 			assert.deepEqual(expected, actual);
 		})
 	});
+	
 	describe('chooseCRT', function() {
-		var factorization = [27, 11, 13, 37];
 		it('should return correct value', function () {
 			var result = ncr.chooseCRT(84, 66, factorization);
-			assert.equal(63687, result);
+			assert.equal(answer, result);
 		})
 	})
+	
 	describe('generalizedLucas', function () {
-		var modulo = 27;
+		var modulo = factorization[0];
 		it('should exist', function() {
 			assert.equal(true, ncr.hasOwnProperty('generalizedLucas'));
 		});
 		it('should return correct value', function() {
-			assert.equal(9, ncr.generalizedLucas(84, 66, modulo));
+			assert.equal(9, ncr.generalizedLucas(84, 66, modulo.prime, modulo.power));
+		});
+	})
+	
+	describe('modularInverse', function () {
+		var modulo = 27, x = 5;
+		it('should exist', function () {
+			assert.equal(true, ncr.hasOwnProperty('modularInverse'));
+		});
+		it('should return correct value', function () {
+			assert.equal(11, ncr.modularInverse(5, 27));
+			assert.equal(4, ncr.modularInverse(3, 11));
 		});
 	})
 })
